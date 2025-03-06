1551b7e7fcc7c09922a0994a90dc3955e30dbc04
olusegun <olusegunbamgbelu755@gmail.com>
2025-03-06 16:19:13 +0100

    time to use useContext for state management
    i will have to create a new branch to implement this feature

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -11,13 +11,13 @@ export default function Header(props) {
 
     const menuLinks = [{id:1, to: "about", text: 'About'}, {id:2, to: "about", text: 'Projects'}, {id:3, to: "about", text: 'Contact'}]
 
-    const displayMenuLinks = menuLinks.map(link => <Link key={link.id} to={`${link.to}`} className="relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-full after:h-0.5 after:bg-red-500 after:opacity-0 hover:after:opacity-100 after:transition-opacity after:duration-300 after:delay-200 pb-[5px] text-base dark:text-gray-200 tracking-wide leading-5 cursor-pointer">{link.text}</Link>) 
+    const displayMenuLinks = menuLinks.map(link => <Link key={link.id} to={`${link.to}`} className="relative after:content-[''] after:absolute after:left-0 after:bottom-0 after:w-full after:h-0.5 after:bg-red-500 after:opacity-0 hover:after:opacity-100 after:transition-opacity after:duration-300 after:delay-200 pb-[5px] text-base dark:text-gray-200 tracking-wide leading-5 cursor-pointer text-center">{link.text}</Link>) 
     
     return (
-        <header className="py-3 px-4 sm:px-8 supports-backdrop-blur:bg-[hsl(240_10%_3.9%_/_0.6)] fixed top-0 left-0 z-50 w-full border-b border-gray-500 backdrop-blur mb-[69px]">
-            <nav className="navbar flex justify-between items-center">
+        <header className="py-3 px-4 sm:px-8 supports-backdrop-blur:bg-[hsl(240_10%_3.9%_/_0.6)] fixed top-0 left-0 z-50 w-full border-b border-gray-500 backdrop-blur mb-[69px] flex justify-center">
+            <nav className="navbar flex items-center w-full justify-center">
                 <img src="../images/kratos.png" alt="logo" className="w-11 h-11 sm:w-13 sm:h-13" />
-                <div className="desktopMenu sm:flex gap-6 hidden">
+                <div className="desktopMenu sm:flex gap-6 hidden mx-auto">
                     {displayMenuLinks}
                 </div>
                 <div className="flex"> 
