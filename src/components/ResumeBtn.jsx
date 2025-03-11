d0d1d533c62ef3e19580b3ffed3bbee29a7a3b58
olusegun <olusegunbamgbelu755@gmail.com>
2025-03-11 09:45:41 +0100

    resume button done

--- src/components/ResumeBtn.jsx
+++ src/components/ResumeBtn.jsx
@@ -1,17 +1,9 @@
 export default function ResumeBtn() {
   return (
-    <div>
-      <a
-        className="relative focus:outline-none capitalize"
-        href=""
-        target="_blank"
-        rel="noreferrer"
-      >
-        <span className="relative block transition-all duration-300 ease-in-out border rounded-md border-red-500 hover:-translate-x-1 hover:-translate-y-1 py-3 px-10 bg-navy-green">
-          Resume
-        </span>
-        <span className="absolute inset-0 rounded-md -z-10 bg-red-800"></span>
-      </a>
-    </div>
+    <a href="">
+      <button className="relative block mr-7 rounded-md border border-red-800 px-10 py-3 text-[18px] leading-[26px] tracking-[-0.16px] transition duration-100 ease-linear hover:shadow-[3px_2px_0_0_#991b1b] hover:-translate-x-1 hover:-translate-y-1">
+        Resume
+      </button>
+    </a>
   );
 }
--- src/layouts/About.jsx
+++ src/layouts/About.jsx
@@ -1,19 +1,20 @@
-import ReachMeBtn from "../components/ReachMeBtn";
+// import ReachMeBtn from "../components/ReachMeBtn";
 /* eslint-disable react/no-unescaped-entities */
-// import ResumeBtn from "../components/ResumeBtn";
+import ResumeBtn from "../components/ResumeBtn";
 
 export default function About() {
     return (
-        <section className="mt-28 sm:mt-[180px] w-4/5 mx-auto py-5">
+        <section className="mt-28 sm:mt-36 w-4/5 mx-auto py-5">
             <div className="aboutme-content flex flex-col items-center">
                 <span className="dark:text-white font-medium border px-4 py-1 rounded-full">
                     Hello 👋🏾 ,
                 </span>
                 <h1 className="text-3xl sm:text-6xl font-bold text-center my-6">I'm Olusegun ⛩</h1>
                 <img src="../images/kratos.png" alt="kratos" className="w-24 h-24 sm:w-36 sm:h-36 mb-7"/>
-                <p className="dark:text-gray-200 text-lg max-w-[600px] text-center mb-7">A front-end developer who works with the React Ecosystem. I make amazing user interfaces come to life...</p>
+                <p className="dark:text-gray-200 text-lg max-w-[600px] text-center mb-10">A front-end developer who works with the React Ecosystem. I make amazing user interfaces come to life...</p>
 
-                <ReachMeBtn />
+                {/* <ReachMeBtn /> */}
+                <ResumeBtn/>
             </div>
         </section>
     );
